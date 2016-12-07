ed2a72a30f9c7c0f42ab1a570e51c5455a39b727
Troy de Freitas <troy.defreitas@uptake.com>
2016-12-07 13:26:35 -0600

    Group lines by field index into HashMap

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use std::io;
 use std::env;
 use std::fs;
 use std::io::*;
+use std::collections::HashMap;
 
 fn main() {
     let input = env::args().nth(1).unwrap_or(String::from("-"));
+    let mut dict : HashMap<String, Vec<String>> = HashMap::new();
+    let keycol = 4;
 
     let rdr: Box<io::BufRead> = match input.as_ref() {
         "-" => Box::new(BufReader::new(io::stdin())),
@@ -14,8 +17,15 @@ fn main() {
     for line in rdr.lines() {
         // Rust *really* wants us to create a temporary let binding
         let line = line.unwrap();
-        let v : Vec<&str> = line.split(",").collect();
-        println!("{}", v[1]);
+        let l = line.clone();
+        let mut split_line = l.split(",");
+        let key = split_line.nth(keycol).unwrap();
+        let value = dict.entry(key.to_owned()).or_insert(Vec::new());
+        value.push(line);
+    }
+
+    for k in dict.keys() {
+        println!("{}|{}", k, dict[k].join("|"));
     }
 }
 
