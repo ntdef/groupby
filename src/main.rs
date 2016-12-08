c32757fa448144addd7d67473b7193cdb11d01f6
ntdef <me@ntdef.com>
2016-12-08 00:24:09 -0600

    Update to rough working version

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
 use std::io;
 use std::env;
 use std::fs;
-use std::io::*;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::io::Write;
+// use std::error::Error;
+// use std::io::stdout;
 use std::collections::HashMap;
+use std::process::{Command, Stdio};
 
-
-fn nth_field(s : &str, n : usize) -> String {
+/// # Examples
+/// ```
+/// use::nth_field;
+///
+/// assert_eq!(nth_field("one, two", 0), "one");
+///
+/// ```
+pub fn nth_field(s : &str, n : usize) -> String {
     s.split(",").nth(n).unwrap().to_owned()
 }
 
 fn main() {
     let input = env::args().nth(1).unwrap_or(String::from("-"));
     let mut dict : HashMap<String, Vec<String>> = HashMap::new();
-    let keycol = 4;
+    let keycol = 2;
 
     let rdr: Box<io::BufRead> = match input.as_ref() {
         "-" => Box::new(BufReader::new(io::stdin())),
         _   => Box::new(BufReader::new(fs::File::open(input).unwrap()))
     };
 
+    // TODO Convert to while loop buffered read.
+    // See: https://doc.rust-lang.org/std/io/trait.BufRead.html#method.read_line
     for line in rdr.lines() {
         // Rust *really* wants us to create a temporary let binding
         let line = line.unwrap();
@@ -28,11 +41,23 @@ fn main() {
     }
 
     for k in dict.keys() {
-        println!("{}|{}", k, dict[k].join("|"));
+        let mut cmd = Command::new("wc")
+            .arg("-l")
+            .stdin(Stdio::piped()).spawn().unwrap();
+        // println!("{}|{}", k, dict[k].join("|"));
+        cmd.stdin.as_mut().unwrap().write_all(dict[k].join("\n").as_bytes()).ok();
+        let out = cmd.wait_with_output().unwrap().stdout;
+        print!("{}", String::from_utf8_lossy(&out));
     }
 }
 
 #[cfg(test)]
 mod test {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(nth_field("Cool, beans, yo", 0), "Cool");
+    }
 
 }
