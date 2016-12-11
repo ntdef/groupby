2db5014f9a34cec0ac8059f7a350d86975d83656
ntdef <me@ntdef.com>
2016-12-11 00:07:46 -0600

    Cleanup, fill out basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
 use std::io;
 use std::env;
 use std::fs;
-use std::io::BufReader;
-use std::io::BufRead;
-use std::io::Write;
-use std::io::Read;
-use std::result;
-use std::iter::Peekable;
-// use std::error::Error;
-// use std::io::stdout;
-use std::collections::HashMap;
+use std::io::{BufReader,BufRead,Write,Read};
 use std::process::{Command, Stdio};
 
 /// # Examples
@@ -19,12 +11,20 @@ use std::process::{Command, Stdio};
 /// assert_eq!(nth_field("one, two", 0), "one");
 ///
 /// ```
-pub fn nth_field(s : &str, n : usize) -> String {
-    s.split(",").nth(n).unwrap().to_owned()
+pub fn nth_field(s : &str, separator : &str, n : usize) -> String {
+    s.split(separator).nth(n).unwrap().to_owned()
 }
 
 fn flush_buffer(buf : &mut String) {
-    println!("{}", buf);
+    let mut bufout = String::new();
+    let cmd = Command::new("bash").arg("-c").arg("wc -l")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    cmd.stdin.unwrap().write_all(buf.as_bytes()).ok();
+    cmd.stdout.unwrap().read_to_string(&mut bufout).ok();
+    print!("{}", bufout);
     buf.clear();
 }
 
@@ -32,105 +32,38 @@ fn main() {
     let input = env::args().nth(1).unwrap_or(String::from("-"));
     // let dict : HashMap<String, Vec<String>> = HashMap::new();
     let keycol = 4;
+    let separator = ",";
 
-    let mut rdr: Box<io::BufRead> = match input.as_ref() {
+    let rdr: Box<io::BufRead> = match input.as_ref() {
         "-" => Box::new(BufReader::new(io::stdin())),
         _   => Box::new(BufReader::new(fs::File::open(input).unwrap()))
     };
-
-    // TODO Convert to while loop buffered read.
-    // See: https://doc.rust-lang.org/std/io/trait.BufRead.html#method.read_line
-    // let mut buffer = String::new();
-    // while rdr.read_line(&mut buffer).unwrap() > 0 {
-    //     // work with buffer
-    //     println!("{:?}", buffer);
-    //     buffer.clear();
-    // }
-
     let mut prev = String::new();
-    let mut buf = String::new();
-    let mut bufout = String::new();
-    let mut itr = rdr.lines();
-    let mut cmd = Command::new("bash")
-        .arg("-c")
-        .arg("wc -l")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
+    let mut buf  = String::new();
+    let mut itr  = rdr.lines();
 
     loop {
         let el = itr.next();
         match el {
             Some(el) => {
                 let line = el.ok().unwrap();
-                let cur = nth_field(&line, keycol);
+                let cur = nth_field(&line, separator, keycol);
                 if cur == prev || prev.is_empty() {
                     buf.push('\n');
                     buf.push_str(&line);
                 } else {
                     flush_buffer(&mut buf);
+                    buf.push('\n');
+                    buf.push_str(&line);
                 }
                 prev = cur
             },
-            // flush buffer
             None => {
                 flush_buffer(&mut buf);
                 break
             },
         }
     }
-
-    return;
-    for line in rdr.lines() {
-        // Rust *really* wants us to create a temporary let binding
-        let line = line.unwrap();
-        let curkey = nth_field(&line, keycol);
-        if curkey != prev {
-            let cmd = Command::new("bash")
-                .arg("-c")
-                .arg("wc -l")
-                .stdin(Stdio::piped())
-                .stdout(Stdio::piped())
-                .spawn()
-                .unwrap();
-            cmd.stdin.unwrap()
-                .write_all(buf.as_bytes()).ok();
-            cmd.stdout.unwrap()
-                .read_to_string(&mut bufout).ok();
-            print!("{}", bufout);
-            bufout.clear(); buf.clear();
-        }
-        prev = curkey;
-        buf.push('\n');
-        buf.push_str(&line);
-        // let value = dict.entry(cur).or_insert(Vec::new());
-        // value.push(line);
-    }
-    let cmd = Command::new("bash")
-        .arg("-c")
-        .arg("wc -l")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
-    cmd.stdin.unwrap()
-        .write_all(buf.as_bytes()).ok();
-    cmd.stdout.unwrap()
-        .read_to_string(&mut bufout).ok();
-    print!("{}", bufout);
-    bufout.clear(); buf.clear();
-    // println!("{} | {}", prev, buf);
-
-    // for k in dict.keys() {
-    //     let mut cmd = Command::new("wc")
-    //         .arg("-l")
-    //         .stdin(Stdio::piped()).spawn().unwrap();
-    //     // println!("{}|{}", k, dict[k].join("|"));
-    //     cmd.stdin.as_mut().unwrap().write_all(dict[k].join("\n").as_bytes()).ok();
-    //     let out = cmd.wait_with_output().unwrap().stdout;
-    //     print!("{}", String::from_utf8_lossy(&out));
-    // }
 }
 
 #[cfg(test)]
@@ -139,7 +72,7 @@ mod test {
 
     #[test]
     fn it_works() {
-        assert_eq!(nth_field("Cool, beans, yo", 0), "Cool");
+        assert_eq!(nth_field("Cool, beans, yo", ",", 0), "Cool");
     }
 
 }
