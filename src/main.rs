bc0f87e281c19c8f8b84bfcc7531cd81c7ce3e55
ntdef <me@ntdef.com>
2016-12-11 00:15:52 -0600

    Rename flush_buffer to exec_with_buffer

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,20 @@ pub fn nth_field(s : &str, separator : &str, n : usize) -> String {
     s.split(separator).nth(n).unwrap().to_owned()
 }
 
-fn flush_buffer(buf : &mut String) {
+fn exec_with_buffer(cmd : &str, buf : &mut String) {
     let mut bufout = String::new();
-    let cmd = Command::new("bash").arg("-c").arg("wc -l")
+    let cmd = Command::new("sh")
+        .arg("-c")
+        .arg(cmd)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
         .unwrap();
+    // Write to stdin
     cmd.stdin.unwrap().write_all(buf.as_bytes()).ok();
+    // Read from stdout, writing to buffer
     cmd.stdout.unwrap().read_to_string(&mut bufout).ok();
     print!("{}", bufout);
-    buf.clear();
 }
 
 fn main() {
@@ -41,6 +44,10 @@ fn main() {
     let mut prev = String::new();
     let mut buf  = String::new();
     let mut itr  = rdr.lines();
+    // TODO: Fix issue with weird inner group line numberings
+    // I'm guessing it's something related to where I introduce
+    // newlines into the buffer.
+    let cmd = "bash -c 'cat -n '";
 
     loop {
         let el = itr.next();
@@ -49,17 +56,18 @@ fn main() {
                 let line = el.ok().unwrap();
                 let cur = nth_field(&line, separator, keycol);
                 if cur == prev || prev.is_empty() {
-                    buf.push('\n');
-                    buf.push_str(&line);
+                    buf.push('\n'); buf.push_str(&line);
                 } else {
-                    flush_buffer(&mut buf);
-                    buf.push('\n');
-                    buf.push_str(&line);
+                    exec_with_buffer(cmd, &mut buf);
+                    buf.clear();
+                    buf.push('\n'); buf.push_str(&line);
                 }
                 prev = cur
             },
             None => {
-                flush_buffer(&mut buf);
+                exec_with_buffer(cmd, &mut buf);
+                // flush_buffer(&mut buf);
+                buf.clear();
                 break
             },
         }
