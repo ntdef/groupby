f0d6de295bbee002eb0ada8f685ee7844dea601f
Troy de Freitas <troy.defreitas@uptake.com>
2016-12-13 17:20:47 -0600

    Final version

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::io;
 use std::fs;
 use std::io::{BufReader,BufRead,Write,Read};
 use std::process::{Command, Stdio};
+
+#[macro_use]
 extern crate clap;
 use clap::{App,Arg};
 
@@ -32,28 +34,16 @@ fn linekey(s : &str, separator : &str, indices : &[Range]) -> String {
     bufout
 }
 
-fn exec_with_buffer(cmd : &str, buf : &mut String) {
-    let mut bufout = String::new();
-    let mut cmd = Command::new("sh")
-        .arg("-c")
-        .arg(cmd)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
-    // Write to stdin
-    cmd.stdin.as_mut().unwrap().write_all(buf.as_bytes()).ok();
-    // Read from stdout, writing to buffer
-    cmd.wait();
-    cmd.stdout.as_mut().unwrap().read_to_string(&mut bufout).ok();
-    print!("{}", bufout);
-}
-
 fn main() {
     let args = App::new("groupby")
         .version("1.0")
         .author("Troy de Freitas <me@ntdef.com>")
         .about("Run a command on groups of lines split by a key.")
+        .arg(Arg::with_name("poolsize")
+             .short("p")
+             .long("pool-size")
+             .help("The number of workers to allow.")
+             .takes_value(true))
         .arg(Arg::with_name("key")
              .short("k")
              .long("key")
@@ -82,6 +72,7 @@ fn main() {
     let input     = args.value_of("input").unwrap_or("-");
     let separator = args.value_of("delimiter").unwrap_or(",");
     let cmd       = args.value_of("command").expect("ACH");
+    let poolsize : usize = value_t!(args, "poolsize", usize).unwrap_or(8);
     let key_range = Range::from_list(key).unwrap();
 
     let rdr: Box<io::BufRead> = match input.as_ref() {
@@ -89,11 +80,11 @@ fn main() {
         _   => Box::new(BufReader::new(fs::File::open(input).unwrap()))
     };
 
-    let mut prev = String::new();
-    let mut buf  = String::new();
-    let mut itr  = rdr.lines();
-
-    let mut process = Process::new(cmd);
+    let mut prev    = String::new();
+    let mut buf     = String::new();
+    let mut itr     = rdr.lines();
+    let mut process = Process::new(cmd, poolsize);
+    let mut work_count : usize = 0;
 
     loop {
         let el = itr.next();
@@ -104,7 +95,7 @@ fn main() {
                 if cur == prev || prev.is_empty() {
                     buf.push_str(&line); buf.push('\n');
                 } else {
-                    // exec_with_buffer(cmd, &mut buf);
+                    work_count+=1;
                     process.push(buf.clone());
                     buf.clear();
                     buf.push_str(&line); buf.push('\n');
@@ -112,13 +103,15 @@ fn main() {
                 prev = cur
             },
             None => {
-                // exec_with_buffer(cmd, &mut buf);
+                work_count+=1;
                 process.push(buf.clone());
                 buf.clear();
                 break
-            },
+            }
         }
-        // for response in process.responses() {println!("{}", response);}
     }
-    // process.join();
+    for i in 0..work_count {
+        let l = process.rx.recv().unwrap();
+        print!("{}", l.unwrap());
+    }
 }
--- src/process.rs
+++ src/process.rs
@@ -8,29 +8,35 @@ use std::io::BufRead;
 use std::io::Write;
 use std::io::Read;
 
+extern crate threadpool;
+
+use self::threadpool::ThreadPool;
+
 pub struct Process {
     // process: std::process::Child,
     tx: mpsc::Sender<Option<String>>,
-    rx: mpsc::Receiver<Option<String>>,
+    pub rx: mpsc::Receiver<Option<String>>,
     cmd: String,
-    children: Vec<JoinHandle<()>>
+    children: Vec<JoinHandle<()>>,
+    pool: ThreadPool,
 }
 
 impl Process {
-    pub fn new(cmd : &str) -> Process {
+    pub fn new(cmd : &str, pool_size : usize) -> Process {
         let (tx, rx) = mpsc::channel();
         Process {
             tx: tx,
             rx: rx,
             cmd: cmd.to_owned(),
             children: vec![],
+            pool: ThreadPool::new(pool_size),
         }
     }
 
     pub fn push(&mut self, buf: String) {
         let tx  = self.tx.clone();
         let cmd = self.cmd.clone();
-        let thrd = thread::spawn(move || {
+        self.pool.execute(move || {
             let mut process =
                 Command::new("sh")
                 .arg("-c")
@@ -43,36 +49,33 @@ impl Process {
                 .expect("unwrapping stdin did not work")
                 .write_all(&buf.as_bytes()).ok();
             let _ = process.wait();
-            let reader = BufReader::new(process.stdout.as_mut().unwrap());
-            for line in reader.lines() {
-                let line = line.unwrap();
-                println!("{}", line.clone());
-                // tx.send(Some(line)).expect("sending to channel failed");
-            }
+            let mut bufout = String::new();
+            process.stdout.as_mut().unwrap().read_to_string(&mut bufout);
+            tx.send(Some(bufout)).expect("sending to channel failed");
+            // let reader = BufReader::new(process.stdout.as_mut().unwrap());
+            // for line in reader.lines() {
+            //     let line = line.unwrap();
+            //     // println!("{}", line.clone());
+            //     tx.send(Some(line)).expect("sending to channel failed");
+            // }
         });
-        self.children.push(thrd);
     }
 
-    pub fn join(&mut self) {
-        while let Some(child) = self.children.pop() {
-            let _ = child.join();
+}
+
+pub struct ProcessIntoIterator<'a> {
+     subprocess: &'a mut Process,
+}
+
+impl <'a>Iterator for ProcessIntoIterator<'a> {
+    type Item = String;
+    fn next(&mut self) -> Option<String> {
+        let data = self.subprocess.rx.try_recv();
+        if data.is_ok() {
+            data.unwrap()
+        }
+        else {
+            None
         }
     }
 }
-
-// pub struct ProcessIntoIterator<'a> {
-//     subprocess: &'a mut Process,
-// }
-
-// impl <'a>Iterator for ProcessIntoIterator<'a> {
-//     type Item = String;
-//     fn next(&mut self) -> Option<String> {
-//         let data = self.subprocess.rx.try_recv();
-//         if data.is_ok() {
-//             data.unwrap()
-//         }
-//         else {
-//             None
-//         }
-//     }
-// }
